3f0118af42f7500d7bd786fd94bcfd1358dd8727
DSagnik24 <sagnikduttaofficial18@gmail.com>
2026-02-18 11:48:35 +0530

    first commit

---
+++ src/Basics/Main1.java
@@ -0,0 +1,22 @@
+package Basics;
+
+// T - type, N - Number, K - key, V - Values, E - Element
+public class Main1 {
+    public static void main(String[] args) {
+        Box box = new Box();
+        box.set(10);
+
+        String s = (String) box.get();
+    }
+}
+
+class Box<T>{
+    Object value;
+
+    void set(Object value){
+        this.value = value;
+    }
+    Object get(){
+        return value;
+    }
+}
